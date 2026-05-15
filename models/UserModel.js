9c46ed379d2186f141e22c6094c960f2b94034a8
AlphonceOtieno <alphonceolilo1@gmail.com>
2026-05-15 10:27:27 +0300

    first commit

---
+++ models/UserModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    }
+}, {
+    timestamps: true
+});
+
+// remove password when sending JSON
+userSchema.methods.toJSON = function () {
+    const user = this.toObject();
+    delete user.password;
+    return user;
+};
+
+module.exports = mongoose.model("User", userSchema);
